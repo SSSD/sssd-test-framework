6c5b2bf32d75489bf97f81615c058c111f23a774
Justin Stephenson <jstephen@redhat.com>
2024-07-29 14:35:49 -0400

    Add IPA IPA Trust Topology Controller
    Add "IPATrustIPA" KnownTopology
    
    For topology groups some changes are:
    
    - Remove "IPATrust"
    - Add "IPATrustAD" -- includes IPATrustAD and IPATrustSamba
    - Add "AnyIPATrust" -- includes IPATrustAD, IPATrustSamba, IPATrustIPA

--- sssd_test_framework/roles/ipa.py
+++ sssd_test_framework/roles/ipa.py
@@ -204,6 +204,13 @@ class IPA(BaseLinuxRole[IPAHost]):
         """
         return f"{name}@{self.domain}"
 
+    @property
+    def admin_fqn(self) -> str:
+        """
+        Return fully qualified admin name in form name@domain.
+        """
+        return f"admin@{self.domain}"
+
     def user(self, name: str) -> IPAUser:
         """
         Get user object.
