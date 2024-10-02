1d31a4939bc7ea055262f3396e1740ef889cf725
Pavel Březina <pbrezina@redhat.com>
2024-10-02 12:05:08 +0200

    utils: use host.cli instead of new instance

--- sssd_test_framework/utils/local_users.py
+++ sssd_test_framework/utils/local_users.py
@@ -31,7 +31,7 @@ class LocalUsersUtils(MultihostUtility[MultihostHost]):
         """
         super().__init__(host)
 
-        self.cli: CLIBuilder = CLIBuilder(host.conn)
+        self.cli: CLIBuilder = host.cli
         self.fs: LinuxFileSystem = fs
         self._users: list[str] = []
         self._groups: list[str] = []
