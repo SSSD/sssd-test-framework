ce8659289026641b028f052fb78c935b93ff5e4e
Dan Lavu <dlavu@redhat.com>
2025-11-24 11:43:39 -0500

    adding delimiter_parser function that has a configurable delimiter

--- sssd_test_framework/misc/__init__.py
+++ sssd_test_framework/misc/__init__.py
@@ -44,6 +44,26 @@ def attrs_parse(lines: list[str], attrs: list[str] | None = None) -> dict[str, l
     return out
 
 
+def delimiter_parse(lines: list[str], delimiter: str = ":") -> dict[str, str]:
+    """
+    Parse delimited lines from output.
+
+    :param lines: Output.
+    :type lines: list[str]
+    :param delimiter: Delimiter, optional
+    :type delimiter: str, defaults to :
+    :return: Dictionary with first element as the name as a key.
+    :rtype: dict[str, str]
+    """
+    out: dict[str, str] = {}
+
+    for item in lines:
+        key, value = item.split(delimiter, 1)
+        out[key.strip()] = value.strip()
+
+    return out
+
+
 def attrs_include_value(attr: Any | list[Any] | None, value: Any) -> list[Any]:
     """
     Include ``value`` to attribute list if it is not yet present.
